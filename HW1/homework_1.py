6226e264a0d8073b412d45e0ed3431dac4657f58
hayhut <67342286+hayhut@users.noreply.github.com>
2020-06-23 13:14:18 -0700

    Create homework_1.py
    Start on user input stuff

---
+++ HW1/homework_1.py
@@ -0,0 +1,22 @@
+import sys
+user_input = ""
+valid_input = ["help", "encrypt", "decrypt", "quit"]
+print("This program will encrypt and decrypt a file made up of english words.\n\n\
+for help, type help.\n\
+for encrypting, type encrypt\n\
+for decrypting, type decrypt\n\
+to quit, type quit.")
+while user_input != "quit":
+
+	raw_input = input("\nEnter selection: ")
+	user_input = sanitize_input(raw_input)
+
+def sanitize_input(raw):
+
+	while (raw in valid_input) != True:
+		print("Invalid selection. For help, type help.\nEnter selection: ")
+		raw = input("\nEnter selection: ")
+	
+	return raw
+	
+#def user_choice(user_input):
