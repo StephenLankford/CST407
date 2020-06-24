55d4f7d163fb06fb119bb298389adcb57f6f5e61
hayhut <67342286+hayhut@users.noreply.github.com>
2020-06-24 14:44:25 -0700

    basic inputs work
    Well duh! can't call a function that doesn't exist yet...

--- HW1/homework_1.py
+++ HW1/homework_1.py
@@ -1,23 +1,27 @@
 import sys
 user_input = ""
 valid_input = ["help", "encrypt", "decrypt", "quit"]
+def sanitize_input(raw):
+
+	while (raw in valid_input) != True: #while the user doesn't type something from the valid_input list
+		print("Invalid selection. For help, type help.\nEnter selection: ")
+		raw = input("\nEnter selection: ")
+	
+	return raw
+
 print("This program will encrypt and decrypt a file made up of english words.\n\n\
 for help, type help.\n\
 for encrypting, type encrypt\n\
 for decrypting, type decrypt\n\
 to quit, type quit.")
-while user_input != "quit":
+
+while user_input != "quit": #stay in the loop until quit is ran
 
 	raw_input = input("\nEnter selection: ")
-	user_input = sanitize_input(raw_input)
+	user_input = sanitize_input(raw_input) #make sure the input is valid
+	if user_input != "quit":
+		action(raw_input)
 
-def sanitize_input(raw):
-
-	while (raw in valid_input) != True:
-		print("Invalid selection. For help, type help.\nEnter selection: ")
-		raw = input("\nEnter selection: ")
-	
-	return raw
 	
 #def user_choice(user_input):
 
