306cce514f8628d16cae4b7efd87854b45bfefab
hayhut <67342286+hayhut@users.noreply.github.com>
2020-08-05 00:36:49 -0700

    Added the basic algorithm
    next task: generate a uint 32 array derived from random digits of pi, with size 18.

--- FinalProject/Blowfish/Blowfish/MainPage.xaml.cs
+++ FinalProject/Blowfish/Blowfish/MainPage.xaml.cs
@@ -30,11 +30,41 @@ namespace Blowfish
         private void EncryptClick(object sender, RoutedEventArgs e)
         {
             //TODO: Encrypt program logic here
+            //Suggestion: have user enter a key in the main menu for both
+            //encryption and decryption.
+            //Left side of data to be encrypted = L,
+            //Right side = R.
+            //TODO: find a way to generate an 18 uint32 array randomly using digits of pi (this is P)
+            //TODO: figure out how user can enter data, and how it can be used in this function.
+            //f is a function, x being the input to it. The function is on the following line
+            //uint32_t h = S[0][x >> 24] + S[1][x >> 16 & 0xff];
+            //return (h ^ S[2][x >> 8 & 0xff]) + S[3][x & 0xff]
+            for (int ii = 0; ii < 16; ii += 2)
+            {
+                L ^= P[ii];
+                R ^= f(L);
+                R ^= P[ii + 1];
+                L ^= f(R);
+            }
+            L ^= P[16];
+            R ^= P[17];
+            //swap L and R here
         }
 
         private void DecryptClick(object sender, RoutedEventArgs e)
         {
             //TODO: Decrypt program logic here
+            //same issues apply in the encrypt function with the p array, f, and user input
+            for (int ii = 0; ii < 16; ii += 2)
+            {
+                L ^= P[ii + 1];
+                R ^= f(L);
+                R ^= P[ii];
+                L ^= f(R);
+            }
+            L ^= P[1];
+            R ^= P[0];
+            //swap L and R
         }
 
         private void RestartClick(object sender, RoutedEventArgs e)
